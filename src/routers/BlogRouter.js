16d3b0e03eeb819e51f6815860d08f2b49728b69
unknown <sarasky.vipper@gmail.com>
2023-11-02 00:01:37 +0700

    updatepf2

--- src/routers/BlogRouter.js
+++ src/routers/BlogRouter.js
@@ -3,7 +3,7 @@ import { body } from "express-validator";
 import BlogController from "../controller/BlogController.js";
 
 const BlogRouter = express.Router();
-
+BlogRouter.get('/public-requested', BlogController.getPublicRequestedPosts);
 BlogRouter.get('/user', BlogController.getBlogsByUser);
 BlogRouter.get("/getall/:search/:limit/:offset/:tag/:sortby/:orderby", BlogController.getAllBlog);
 BlogRouter.post("/react", BlogController.reactBlog);
@@ -15,8 +15,9 @@ BlogRouter.post(
     .withMessage("Title for blog must be at least 1 characters and less than 20 characters"),
   body("content").isLength({ min: 10 }).withMessage("Content for blog must be at least 10 characters"),
   BlogController.createBlog
-);
 
+);
+BlogRouter.put('/requested', BlogController.requestPublic);
 
 
 
